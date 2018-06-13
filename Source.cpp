2db330387ad67c098992f26dac94198e37585878
Ananya <ananyagopal61@gmail.com>
2018-06-13 13:45:32 -0400

    Trees data structure

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class TreeNode {
+private:
+	int data;
+	TreeNode* left;
+	TreeNode* right;
+
+public: 
+	TreeNode(int d) {
+		data = d;
+		left = right = NULL;
+	}
+};
+
+class Solution {
+public:
+	void InorderTraversal(TreeNode* root) {
+	
+
+
+	}
+
+
+
+
+};
+
+int main() {
+
+	TreeNode* root = new TreeNode(1);
+	return 0;
+}
+
+
+
