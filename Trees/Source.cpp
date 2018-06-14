66935a0eb7b935e5355d8ef063421820ae8b2f43
Ananya <ananyagopal61@gmail.com>
2018-06-14 13:37:08 -0400

    Traversals added

--- Trees/Source.cpp
+++ Trees/Source.cpp
@@ -1,14 +1,35 @@
+/*
+TREE: A simple daa structure 
+ - Average running time of most operations is O(log N)
+ - Tree is a collection of nodes, with a distinguished node (root)
+ - Terms: Child, parent, leaves (no children), siblings (same parent), Grandparent, grandchild
+ - Terms: path, length, depth (length of the unique path from the root to node), height (length of longest path to a leaf)
+ - Terms: ancestor, descendent, proper ancestor, proper descendent
+*/
+
 #include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
 
-class TreeNode {
-private:
-	int data;
-	TreeNode* left;
-	TreeNode* right;
 
-public: 
-	TreeNode(int d) {
+class TreeNode{
+public:
+
+
+
+
+};
+
+// Every node has either 0,1 or 2 children
+class BinTreeNode {
+public:
+	int data;
+	BinTreeNode* left;
+	BinTreeNode* right;
+
+	/* Constructor */
+	BinTreeNode(int d) {
 		data = d;
 		left = right = NULL;
 	}
@@ -16,20 +37,50 @@ public:
 
 class Solution {
 public:
-	void InorderTraversal(TreeNode* root) {
-	
+	// Iterative solution for inorder traversal which is left root right
+	vector<int> inorderTraversal(BinTreeNode* root) {
+		vector<int> result;
+		stack<BinTreeNode*> mystack;
+		BinTreeNode* curr = root;
 
+		while (curr != NULL || !mystack.empty()) {
+			while (curr != NULL) {
+				mystack.push(curr);
+				curr = curr->left;
+			}
+			curr = mystack.top();
+			mystack.pop();
 
+			result.push_back(curr->data);
+			curr = curr->right;
+		}
+		return result;
 	}
 
-
-
-
+	// Iterative solution for Preorder traversal
+	vector<int> preorderTraversal(BinTreeNode* root) {
+		vector<int> result;
+		stack<BinTreeNode*> mystack;
+		BinTreeNode* curr = root;
+		while (curr != NULL || !mystack.empty()) {
+			while (curr != NULL) {
+				mystack.push(curr);
+				result.push_back(curr->data);
+				curr = curr->left;
+			}
+			curr = mystack.top();
+			mystack.pop();
+			curr = curr->right;
+		}
+		return result;
+	}
 };
 
 int main() {
 
-	TreeNode* root = new TreeNode(1);
+	BinTreeNode* root = new BinTreeNode(1);
+
+	cout << root->data;
 	return 0;
 }
 
